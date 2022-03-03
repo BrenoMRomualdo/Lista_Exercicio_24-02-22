256036c3b9839d5b250b716c4184d23ffa4e4e76
BrenoMR <brenomromualdo@gmail.com>
2022-03-03 12:39:35 -0300

    Primeiro comit da aula

--- exercicio8.java
+++ exercicio8.java
@@ -1,19 +1,21 @@
 import javax.swing.JOptionPane;
 public class exercicio8 {
     public static void main(String[] args) {
+        int eleitores, votos_branco, votos_nulos, op1, op2;
+        int porcent_1, porcent_2, porcent_3, votos_validos;
 
-        double Salario, porcent_ajuste, Op1, Op2, Novo_salario;
-
-        Salario = Double.parseDouble(JOptionPane.showInputDialog("Qual é o seu salário mensal?"));
-        porcent_ajuste = Double.parseDouble(JOptionPane.showInputDialog("Qual o percentual de ajuste de seu Salário?"));
-        Op1 = porcent_ajuste /100;
-        Op2 = Salario * Op1;
-        Novo_salario = Salario + Op2;
-
+        eleitores = Integer.parseInt(JOptionPane.showInputDialog("Quantos eleitores tem nesse municipio?"));
+        votos_branco = Integer.parseInt(JOptionPane.showInputDialog(eleitores, "Desses %f eleitores quantos votaram em branco?"));
+        op1 = eleitores - votos_branco;
+        votos_nulos = Integer.parseInt(JOptionPane.showInputDialog(op1, "Desses %f eleitores restantes, quantos votaram nulo?"));
+        op2 = op1 - votos_nulos;
+        votos_validos = eleitores - op2;
+        porcent_1 = (votos_branco * 100) / eleitores;
+        porcent_2 = (votos_nulos * 100) / eleitores;
+        porcent_3 = (votos_validos *100) / eleitores; 
+        
         String s = String.format(
-            "O seu salário antigo era %.2f, após o ajuste seu salário é = %.2f!",
-            Salario, Novo_salario
+            "Neste municipio tem %f eleitores"
         );
-        JOptionPane.showMessageDialog(null, s);
     }    
 }
